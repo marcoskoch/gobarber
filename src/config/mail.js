1ec390812671a2c9e422082203b00e910dc0455c
Marcos Koch <marcos_koch@hotmail.com>
2020-01-27 22:44:21 -0300

    add project

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '1b98eb78e5f98b',
+    pass: '6c4af417560180',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreplay@gobarber.com>',
+  },
+};
