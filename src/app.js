b4b6b5713ddaeddd44136cabb917bc637d6ecdf4
Marcos Koch <marcos_koch@hotmail.com>
2020-01-29 22:44:30 -0300

    finish sentry config

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import path from 'path';
+import Youch from 'youch';
 import * as Sentry from '@sentry/node';
 import sentryConfig from './config/sentry';
-import routes from './routes';
 import 'express-async-errors';
+import routes from './routes';
 import './database';
 
 class App {
@@ -14,6 +15,7 @@ class App {
 
     this.middlewares();
     this.routes();
+    this.exceptionHandler();
   }
 
   middlewares() {
@@ -28,6 +30,14 @@ class App {
   routes() {
     this.server.use(routes);
     this.server.use(Sentry.Handlers.errorHandler());
+  }
+
+  exceptionHandler() {
+    this.server.use(async (err, req, res, next) => {
+      const errors = await new Youch(err, req).toJSON();
+
+      return res.status(500).json(errors);
+    });
   }
 }
 
